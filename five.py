f1779c10508a44fea6473b6f27f4fa09e25171b2
sri112233 <51362255+sri112233@users.noreply.github.com>
2019-07-24 23:43:28 -0700

    Create five.py

---
+++ five.py
@@ -0,0 +1,12 @@
+x = float(input("Enter first number: "))
+y = float(input("Enter second number: "))
+z = float(input("Enter third number: "))
+ 
+if (x > y) and (x > z):
+   largest = x
+elif (y > x) and (y > z):
+   largest = y
+else:
+   largest = z
+ 
+print("The largest number is",largest)
